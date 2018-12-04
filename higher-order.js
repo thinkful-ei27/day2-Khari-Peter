85196e49426197d4bc3e493490c32ab050c5772c
kriviears <kriviears@gmail.com>
2018-12-04 14:41:53 -0500

    Filter solved, can also return index of filtered items. Also invoked function and logged in single line

--- higher-order.js
+++ higher-order.js
@@ -14,3 +14,31 @@ function goodbye () {
 
 repeat(hello, 5);
 repeat(goodbye, 5);
+
+
+
+function filter(arr, fn) {
+  // TASK: Define your function here
+  let newArray = [];
+  for(let i=0; i<arr.length; i++){
+    if(fn(arr[i], i, arr)){
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
+}
+
+// DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
+// First we setup an array of strings we plan to filter:
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+// We use your `filter` function here, capturing a new array into `filteredNames`
+// comprised of names that only begin with 'R'
+const filteredNames = filter(myNames, function(name) {
+  // This is known as a "predicate function" - it's a function that 
+  // only returns a boolean
+  return name[0] === 'R';
+});
+
+console.log(filteredNames) // => ['Rich', 'Ray']
+console.log(filter(myNames, name => name[0]==='R'));
