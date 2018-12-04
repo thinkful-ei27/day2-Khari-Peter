c8db63f1aa6a1808a65a32a586b986c8f12640fb
kriviears <kriviears@gmail.com>
2018-12-04 12:21:14 -0500

    max and min solution modified for while loop

--- loops.js
+++ loops.js
@@ -1,1 +1,28 @@
 console.log('hello world');
+
+
+function max(numbers) {
+    // your code here
+    let result = numbers[0];
+    let i = 0; 
+    while (i<numbers.length){ 
+        if(numbers[i]>result){
+            result = numbers[i];
+        }
+        i++;
+    }
+    return result;
+}
+
+function min(numbers) {
+    // your code here
+    let result = numbers[0];
+    let i = 0; 
+    while (i<numbers.length){ 
+        if(numbers[i]<result){
+            result = numbers[i];
+        }
+        i++;
+    }
+    return result;
+}
