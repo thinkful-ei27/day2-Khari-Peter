ddc0d9722d2373934f2d3172392caefe43ba99c9
Peter <peterdramos02@gmail.com>
2018-12-04 14:14:49 -0500

    make hello and good funcs and call each w/ repeat func

--- higher-order.js
+++ higher-order.js
@@ -3,3 +3,14 @@ function repeat (fn, n) {
     fn();
   }
 }
+
+function hello () {
+  console.log('Hello World');
+}
+
+function goodbye () {
+  console.log('Goodbye World');
+}
+
+repeat(hello, 5);
+repeat(goodbye, 5);
